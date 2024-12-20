ca4203cbb440b2f1508b4c689e58878055a0adab
Tyr <langtuananh2424@gmail.com>
2024-12-20 20:24:29 +0700

    Add comment

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Borrow;
 
 class Book extends Model
 {
-    //
+    // Used to find all borrowings of the book
+    public function borrows() {
+        return $this->hasMany(Borrow::class);
+    }
 }
--- app/Models/Reader.php
+++ app/Models/Reader.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Borrow;
 
 class Reader extends Model
 {
-    //
+    // Find all the books borrowed by a reader.
+    public function borrows() {
+        return $this->hasMany(Borrow::class);
+    }
 }
